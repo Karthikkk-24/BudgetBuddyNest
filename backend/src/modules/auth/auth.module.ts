64808fec590333296be62321ca4bfd35cf4bf0f3
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-10-19 23:34:48 +0530

    Changes to authModule

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,6 +1,18 @@
 import { Module } from "@nestjs/common";
-import { AuthController } from "../../auth/auth.controller";
-import { AuthService } from "../../auth/auth.service";
-import { TokensModule } from "../../tokens/tokens.module";
-import { UsersModule } from "../../users/users.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { AuthToken, AuthTokenSchema } from "./schemas/auth-token.schema";
+import { User, UserSchema } from "./schemas/user.schema";
 
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: AuthToken.name, schema: AuthTokenSchema },
+    ]),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
